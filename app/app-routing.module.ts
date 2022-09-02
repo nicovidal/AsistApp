d383eed32a1c6da0a4137fe8e865e40146e6c608
Nicolas Vidal <nico.vidal@duocuc.cl>
2022-09-02 15:06:43 -0400

    login

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'inicio',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'asist-qr',
     loadChildren: () => import('./pages/asist-qr/asist-qr.module').then( m => m.AsistQRPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
 ];
 
