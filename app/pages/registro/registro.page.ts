3f11b591a494e2cfeee4d03f0eff45bbf44dc29e
Nicolas Vidal <nico.vidal@duocuc.cl>
2022-09-05 01:49:45 -0300

    boton back login ok!

--- app/pages/registro/registro.page.ts
+++ app/pages/registro/registro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  constructor(private menuController:MenuController) { }
 
   ngOnInit() {
   }
-
+  mostrarMenu(){
+    this.menuController.open('first');
+  }
 }
