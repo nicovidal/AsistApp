a654958e0048c6b96500666af3ecbd21752f29ae
Nicolas Vidal <nico.vidal@duocuc.cl>
2022-10-19 13:23:08 -0300

    login reactivo ok!

--- app/pages/splash/splash.page.ts
+++ app/pages/splash/splash.page.ts
@@ -12,7 +12,7 @@ export class SplashPage implements OnInit {
 
   ngOnInit() {
     setTimeout(()=>{
-      this.router.navigateByUrl('bienveni');
+      this.router.navigateByUrl('login');
     },2700);
   }
 
